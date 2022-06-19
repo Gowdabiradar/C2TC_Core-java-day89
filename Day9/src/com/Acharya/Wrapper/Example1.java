9a824ed89a84837b69abb74302adb7cca5345d91
Prajwal <Prajwal@DESKTOP-O8NF7C0>
2022-06-19 23:42:21 +0530

    First commit

---
+++ Day9/src/com/Acharya/Wrapper/Example1.java
@@ -0,0 +1,15 @@
+package com.Acharya.Wrapper;
+
+public class Example1 {
+
+		public static void main(String[] args) {
+	           int i=41;
+	           Integer a= Integer.valueOf(i);
+	           Integer b=i;
+	           System.out.println(i+" "+a+" "+b);
+
+		}
+
+	}
+
+
---
+++ Day9/src/com/Acharya/Wrapper/Example3.java
@@ -0,0 +1,11 @@
+package com.Acharya.Wrapper;
+public class Example3 {
+
+	public static void main(String[] args) {
+		 Integer a=new Integer(3);
+		 int i=a.intValue();
+		 int j=a;
+
+	}
+
+}
